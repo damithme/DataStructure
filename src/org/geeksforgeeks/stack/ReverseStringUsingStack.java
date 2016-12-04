7598797dc12ed3fb0106d074004e200d1d084693
Damith <damithme@gmail.com>
2016-12-04 18:39:57 +0530

    added sort using stack.

--- src/org/geeksforgeeks/stack/ReverseStringUsingStack.java
+++ src/org/geeksforgeeks/stack/ReverseStringUsingStack.java
@@ -1,7 +1,90 @@
 package org.geeksforgeeks.stack;
 
 /**
+ * Reverse string using stack.
+ *
+ * Stack | Set 3 (Reverse a string using stack)
+ * http://quiz.geeksforgeeks.org/stack-set-3-reverse-string-using-stack/
+ *
  * Created by DAM on 11/28/16.
  */
 public class ReverseStringUsingStack {
+
+    private int top;
+    private int capacity;
+    private char[] array;
+
+    public ReverseStringUsingStack(int capacity){
+        this.capacity = capacity;
+        this.array = new char[capacity];
+        this.top = -1;
+    }
+
+    /**
+     * check stack is full or not.
+     * @return
+     */
+    public boolean isFull(){
+        return this.top == this.capacity - 1;
+    }
+
+    /**
+     * check stack is empty.
+     * @return
+     */
+    public boolean isEmpty(){
+        return this.top == -1;
+    }
+
+    /**
+     * stack push.
+     *
+     * @param value
+     */
+    public void push(char value){
+        if (this.isFull()) {
+            return;
+        }
+        this.array[++this.top] = value;
+    }
+
+    /**
+     * stack pop.
+     *
+     * @return
+     */
+    public char pop(){
+        if (this.isEmpty()) {
+            return '\u0000';
+        }
+        return this.array[this.top--];
+    }
+
+    /**
+     * reverse given string.
+     *
+     * @param str
+     * @return
+     */
+    public static String reverse(String str){
+        char[] charArr = str.toCharArray();
+        int size = charArr.length;
+
+        ReverseStringUsingStack stack = new ReverseStringUsingStack(size);
+
+        for (int i=0; i < size; i++) {
+            stack.push(charArr[i]);
+        }
+
+        for (int i=0; i < size; i++) {
+            charArr[i] = stack.pop();
+        }
+
+        return String.valueOf(charArr);
+    }
+
+    public static void main(String[] args) {
+        String str = "GeeksQuiz";
+        System.out.println(reverse(str));
+    }
 }
--- src/org/geeksforgeeks/stack/SortAStack.java
+++ src/org/geeksforgeeks/stack/SortAStack.java
@@ -1,7 +1,115 @@
 package org.geeksforgeeks.stack;
 
 /**
- * Created by DAM on 12/4/16.
+ * Linked list based Stack implementation.
+ *
+ * Stack | Sort a stack using recursion.
+ * http://www.geeksforgeeks.org/sort-a-stack-using-recursion/
+ *
+ * Created by Damith Ganegoda on 11/24/16.
  */
+
 public class SortAStack {
+    class StackNode {
+        int data;
+        StackNode next;
+    }
+
+
+    private StackNode root;
+
+    /**
+     * check Stack is empty or not.
+     * @return boolean
+     */
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+    /**
+     * Push a data to Stack.
+     * @param data
+     */
+    public void push(int data){
+        StackNode stackNode = new StackNode();
+        stackNode.data = data;
+        stackNode.next = root;
+
+        this.root = stackNode;
+        //System.out.println(String.format("%d pushed to stack.", data));
+    }
+
+    /**
+     * Remove a data from Stack.
+     * @return
+     */
+    public int pop(){
+        if (isEmpty()) {
+            return Integer.MIN_VALUE;
+        }
+
+        StackNode tempNode = this.root;
+        this.root = tempNode.next;
+
+        return tempNode.data;
+    }
+
+    /**
+     * Get top value in Stack.
+     * @return
+     */
+    public int top(){
+        if (this.root == null) {
+            return Integer.MIN_VALUE;
+        }
+        return this.root.data;
+    }
+
+    /**
+     * sort the stack.
+     * @param stack
+     */
+    public void sortStack(SortAStack stack){
+        if (!stack.isEmpty()) {
+            int tempData = stack.pop();
+            sortStack(stack);
+            sortedInsert(stack, tempData);
+        }
+    }
+
+    public void sortedInsert(SortAStack stack, int data){
+        if (stack.isEmpty() || data > stack.top()) {
+            stack.push(data);
+        }
+        else {
+            int temp = stack.pop();
+            sortedInsert(stack, data);
+            stack.push(temp);
+        }
+    }
+
+    /**
+     * print all elements in stack.
+     */
+    public void print(){
+        StackNode node = this.root;
+        while (node != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        SortAStack stack = new SortAStack();
+        stack.push(39);
+        stack.push(50);
+        stack.push(10);
+        stack.push(6);
+        stack.push(4);
+        stack.print();
+        System.out.println("----------");
+        stack.sortStack(stack);
+        stack.print();
+    }
 }
